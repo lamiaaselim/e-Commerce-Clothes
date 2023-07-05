deea50148efa0a8bdd9a8faabb28eac531939430
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-05 23:56:42 +0300

    niceScroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
 // Nice Scrolling
 $("html").niceScroll({
     cursorwidth: '12px',
+    cursorminheight: 32,
     cursorcolor: "black",
     cursorborder:"2px solid rgb(208, 77, 30)",
     cursorborderradius:"5px",
     autohidemode: false, 
-    scrollspeed: 100,
-    zindex: 999
+    // scrollspeed: 1000,
+    smoothscroll:true,
+    enablekeyboard:true,
+    enablemousewheel:true,
+    // mousescrollstep: 8 * 3,
+    zindex:"auto",
 }); 
