5c1c632b88f7cdb0f7a3b3e2c04bf79065888702
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-05 23:40:26 +0300

    bootstrap problem

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// Nice Scrolling
+$("html").niceScroll({
+    cursorwidth: '12px',
+    cursorcolor: "black",
+    cursorborder:"2px solid rgb(208, 77, 30)",
+    cursorborderradius:"5px",
+    autohidemode: false, 
+    scrollspeed: 100,
+    zindex: 999
+}); 
