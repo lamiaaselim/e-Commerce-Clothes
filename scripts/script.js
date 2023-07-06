645887b8bab701e4c35da08388f2f3634fc3280b
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-06 04:18:52 +0300

    finish

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,9 @@ $("html").niceScroll({
     cursorborder:"2px solid rgb(208, 77, 30)",
     cursorborderradius:"5px",
     autohidemode: false, 
-    // scrollspeed: 1000,
     smoothscroll:true,
+    
     enablekeyboard:true,
     enablemousewheel:true,
-    // mousescrollstep: 8 * 3,
-    zindex:"auto",
+    zindex:999,
 }); 
