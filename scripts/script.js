f03af839dc58957c86dbdf27918e43767d919b03
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-07 04:01:19 +0300

    removeNiceScroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-// Nice Scrolling
-$("html").niceScroll({
-    cursorwidth: '12px',
-    cursorminheight: 32,
-    cursorcolor: "black",
-    cursorborder:"2px solid rgb(208, 77, 30)",
-    cursorborderradius:"5px",
-    autohidemode: false, 
-    smoothscroll:true,
+// // Nice Scrolling
+// $("html").niceScroll({
+//     cursorwidth: '12px',
+//     cursorminheight: 32,
+//     cursorcolor: "black",
+//     cursorborder:"2px solid rgb(208, 77, 30)",
+//     cursorborderradius:"5px",
+//     autohidemode: false, 
+//     smoothscroll:true,
     
-    enablekeyboard:true,
-    enablemousewheel:true,
-    zindex:999,
-}); 
+//     enablekeyboard:true,
+//     enablemousewheel:true,
+//     zindex:999,
+// }); 
